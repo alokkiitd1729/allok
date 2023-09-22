6d0f7f0e410b1be7ea555dd6d1dadff6e66e4bb6
Alok Kumar <alokjnvl41@gmail.com>
2023-09-23 02:12:02 +0530

    Restored hello.go from head~2 to staging area using 'git restore --staged --source=head~2 hello.go'  and then from staging to working are using 'git restore hello.go'   Now hello.go will be appearing to cmmit area also after this commit

---
+++ dev/hello/hello.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+type test struct {
+	ok  int
+	ok1 int
+}
+
+func NewTest(v int) *test {
+	return &test{
+		ok:  v,
+		ok1: v * v,
+	}
+}
+func greetings(name string) {
+	fmt.Printf("Hi there!! %s", name)
+}
+
+func main() {
+	tmp := NewTest(4)
+	fmt.Println(tmp)
+	fmt.Printf("%p\n", tmp)
+	fmt.Println(tmp.ok)
+	fmt.Println(*tmp)
+	fmt.Println(&tmp)
+}
